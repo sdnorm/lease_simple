5282f608099f5828aa43fafb1dd01e6b16f97d7a
Spencer Norman <spencerdnorman@gmail.com>
2019-04-22 22:25:33 -0500

    added a navbar and the routes controllers and view files for the beginning general pages

---
+++ app/controllers/general_pages_controller.rb
@@ -0,0 +1,18 @@
+class GeneralPagesController < ApplicationController
+
+  def index
+  end
+
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def terms_of_service
+  end
+
+  def faq
+  end
+
+end
